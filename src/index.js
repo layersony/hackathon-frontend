6afc47a9a7ad2dce1b315b038aae6f29761a18d0
layersony <layersony@gmail.com>
2022-09-04 14:41:31 +0300

    deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,9 +11,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 

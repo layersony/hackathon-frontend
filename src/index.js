b15223c042c45cc7293977b244a0983ad86203a4
layersony <layersony@gmail.com>
2023-01-19 06:53:23 +0300

    homepage url change

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
